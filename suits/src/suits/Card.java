ce1bbc0eee29e897c6552f6bceafa46712d5d8f9
JoshyBoye <mcrafterx@gmail.com>
2023-03-08 03:39:57 +0000

    Initial commit

---
+++ suits/src/suits/Card.java
@@ -0,0 +1,48 @@
+package suits;
+
+public class Card{	  	  		      	     	      	     	
+ 	     	
+	  private String suit;	  	  		      	     	      	     	
+	  private String cardNumber;	  	  		      	     	      	     	
+	  private int value;	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public void setSuit(String suit)	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	     this.suit = suit;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public String getSuit()	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	     return this.suit;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public void setCardNumber(String cardNumber)	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	     this.cardNumber = cardNumber;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public String getCardNumber()	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	     return this.cardNumber;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public void setValue(int Value)	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	     this.value = Value;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public int getValue()	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	    return this.value;	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+		  	  		      	     	      	     	
+	  public int getValue(boolean AceAbility)	  	  		      	     	      	     	
+	  {	  	  		      	     	      	     	
+	    if (AceAbility == true && this.cardNumber == "Ace"){	  	  		      	     	      	     	
+	      return 11;	  	  		      	     	      	     	
+	    }else {	  	  		      	     	      	     	
+	      return this.value;	  	  		      	     	      	     	
+	    }	  	  		      	     	      	     	
+	  }	  	  		      	     	      	     	
+	}	  	  		      	     	      	     	
+
