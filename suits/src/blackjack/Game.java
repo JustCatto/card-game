82f9674a4b93da6de4090437ec03988409c77722
JustCatto <mcrafterx@gmail.com>
2023-03-10 15:31:21 +0000

    Blackjack 2nd commit

--- suits/src/blackjack/Game.java
+++ suits/src/blackjack/Game.java
@@ -1,6 +1,7 @@
 package blackjack;
 import cardpile.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
 	
@@ -8,27 +9,79 @@ public class Game {
 	private ArrayList<Hand> players = new ArrayList<Hand>();
 	
 	public void game() {
-		
+		boolean gameend = false;
+		while (gameend != true) {
+			gameend = true;
+			for (int i = 0;i<players.size();i++) {
+				Hand currentPlayer = players.get(i);
+				if (!currentPlayer.isFinished()){
+					System.out.println("It is player- "+currentPlayer.getDeckName()+"'s Turn.");
+					System.out.println("Deck Value- "+currentPlayer.getValue());
+					if (this.getDrawDecision() == true) {
+						this.drawCard(currentPlayer);
+						gameend = false;
+					} else {
+						currentPlayer.setFinished(true);  
+					}
+				}
+			}
+		}
 	}
 	
-	public void startGame() {
-		deck.populateDeck();
-		deck.shuffleDeck();
-		
+	public void getWinners(){
+		ArrayList<Hand> winners = new ArrayList<Hand>();
+		int min = players.get(0).getValue()-21;
+		for (int i = 1;i<this.players.size();i++) {
+			
+		}
+	}
+	
+	public boolean getDrawDecision() {
+		Scanner input = new Scanner(System.in);
+		boolean valid = false;
+		while (valid == false) {
+			String decision = "";
+			System.out.println("Please input if you will draw or stick. [D/S]\n-->");
+			decision = input.nextLine();
+			if (decision.equalsIgnoreCase("D")){
+				return true;
+			} else if (decision.equalsIgnoreCase("S")) {
+				return false;
+			} else {
+				System.out.println("Please enter either D or S.\n-->");
+			}
+		}
+		return false;
+	}
+	
+	public void startGame(int playernum) {
+		this.initDeck();
+		this.initPlayers(playernum);
+		this.getStartingCards();
+	}
+	
+	public void getStartingCards() {
+		for (int i = 0;i<this.players.size();i++) {
+			Hand currentPlayer = this.players.get(i);
+			this.drawCard(currentPlayer);
+		}
+	}
+	
+	public void initDeck() {
+		this.deck.populateDeck();
+		this.deck.shuffleDeck();
 	}
 	
 	public void initPlayers(int p) {
 		for (int i = 0;i < p; i++) {
 			Hand player = new Hand("Player "+Integer.toString(i));
-			players.add(player);
+			this.players.add(player);
 		}
 	}
 	
-	
-	
 	public void drawCard(Hand hand) {
 		Card pickedCard = deck.getCard();
-		deck.removeCard(0);
+		this.deck.removeCard(0);
 		hand.addCard(pickedCard);
 	}
 
--- suits/src/cardpile/Hand.java
+++ suits/src/cardpile/Hand.java
@@ -2,19 +2,45 @@ package cardpile;
 
 public class Hand extends Cardpile {
 
+	private boolean finished = false;
+	
 	public Hand(String deckName) {
 		super(deckName);
 	}
 	
-	public void printPile() {
-		
+	public void printHand() {
+		System.out.println(this.deckName+"s Hand-\n\n");
 		for (int i = 0;i<this.pile.size();i++) {
 			Card currentCard = this.pile.get(i);
-			System.out.println(currentCard.getSuit());
-			System.out.println(currentCard.getCardNumber());
-			
+			String suit = currentCard.getSuit();
+			String cardNumber = currentCard.getCardNumber();
+			System.out.print(cardNumber+" Of "+suit+"s\t");
 		}
 	}
+	
+
+	public boolean isFinished() {
+		return finished;
+	}
+
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+	
+
+	public int getValue() {
+		int value = 0;
+		for (int i = 0;i < this.pile.size();i++) {
+			Card currentCard = this.pile.get(i);
+			if (currentCard.getCardNumber().equals("Ace") && value + 11 <= 21) {
+				value += 11;
+			} else {
+				value += currentCard.getValue();
+			}
+		}
+		return value;
+	}
+
 	
 	
 
